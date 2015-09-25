68465da5c83a7194cfec8e81b948b8c69d89e2c1
srichatala <sri2vas@gmail.com>
2015-09-24 22:43:00 -0400

    Intial folders and player, background added to application

---
+++ Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public Vector2 move = new Vector2(0.0f, 0.0f);
+
+    private Vector2 newPosition = new Vector2(0.0f, 0.0f);
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Player into screen");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        newPosition = gameObject.GetComponent<Transform>().position;
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            newPosition.x += this.move.x;
+            gameObject.GetComponent<Transform>().position = newPosition;
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            newPosition.x -= this.move.x;
+            gameObject.GetComponent<Transform>().position = newPosition;
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            newPosition.y += this.move.y;
+            gameObject.GetComponent<Transform>().position = newPosition;
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            newPosition.y -= this.move.y;
+            gameObject.GetComponent<Transform>().position = newPosition;
+        }
+    }
+}
