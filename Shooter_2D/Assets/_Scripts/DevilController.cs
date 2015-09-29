0a4bea34ff1461b5ee1e4bcaccf21d65c6d981a1
srichatala <sri2vas@gmail.com>
2015-09-29 16:42:36 -0400

    devil controller added and gameobject added to generate multiple devils

---
+++ Shooter_2D/Assets/_Scripts/DevilController.cs
@@ -0,0 +1,60 @@
+﻿/*
+Author: Sri Chatala
+File : DavilController.cs
+Created Date: Spe 25,2015
+Descriptopn: Devil controller script controll the Devil gameObject
+Laster Updated: Spe 25,2015
+*/
+using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class Speed
+{
+    public float minSpeed, maxSpeed;
+
+}
+
+//[System.Serializable]
+//public class Drift
+//{
+//    public float minDrift;
+//    public float maxDrift;
+//}
+public class DevilController : MonoBehaviour {
+
+    //Initialize the speed of the balloon
+    public Speed speed;
+
+    //public Drift drift;
+
+    private float _CurrentSpeed;
+
+ //   private float _CurrectDrift;
+    // Use this for initialization
+    void Start () {
+        //call the reset method to move the balloon
+        this._Reset();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 currentPosition = new Vector2(0.0f, 0.0f);
+        currentPosition = gameObject.GetComponent<Transform>().position;
+
+       // currentPosition.y += this._CurrectDrift;
+        currentPosition.x -= this._CurrentSpeed;
+
+        gameObject.GetComponent<Transform>().position = currentPosition;
+        if (currentPosition.x <= -12.536)
+        {
+            this._Reset();
+        }
+    }
+    private void _Reset()
+    {
+        //this._CurrectDrift = Random.Range(drift.minDrift, drift.maxDrift);
+        this._CurrentSpeed = Random.Range(speed.minSpeed, speed.maxSpeed);
+        Vector2 resetPosition = new Vector2(12.536f, Random.Range(-4.1f, 4.1f));
+        gameObject.GetComponent<Transform>().position = resetPosition;
+    }
+}
---
+++ Shooter_2D/Assets/_Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public int devilCount;
+    public GameObject devil;
+	// Use this for initialization
+	void Start () {
+        this._GenerateDevil();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    private void _GenerateDevil()
+    {
+        for(int count = 0; count < devilCount; count++)
+        {
+            Instantiate(devil);
+        }
+    }
+}
