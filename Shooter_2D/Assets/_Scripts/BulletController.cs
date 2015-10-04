c3eaa7e22bb954b6b05b36a50ea2fec147ab2259
srichatala <sri2vas@gmail.com>
2015-10-04 00:03:34 -0400

    bullets added but its not working

---
+++ Shooter_2D/Assets/_Scripts/BulletController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity = transform.forward * Speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
